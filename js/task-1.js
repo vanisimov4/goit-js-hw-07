6426f20c76d04d9d503601ab653b0bc6e114b359
vanisimov4 <144102084+vanisimov4@users.noreply.github.com>
2024-01-10 16:48:52 +0200

    task-2

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,8 @@
 const allItems = document.querySelectorAll('.item');
 console.log(`Number of categories: ${allItems.length}`);
-console.log(`Categories: ${allItems.firstElementChild}`);
-const firstEl = allItems.lastElementChild;
-console.log(allItems.lastElementChild);
 allItems.forEach(items => {
-    console.log(items);
-    // console.log(items.length);
-  console.log(`Categories: ${items.childNodes}`);
+  const title = items.querySelector('h2');
+  console.log(`Category: ${title.textContent}`);
+  const allLi = items.querySelectorAll('li');
+  console.log(`Elements: ${allLi.length}`);
 });
