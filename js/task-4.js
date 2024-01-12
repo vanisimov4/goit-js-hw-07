9016e3b36998b7bdbdeac022b84715451c9b7299
vanisimov4 <144102084+vanisimov4@users.noreply.github.com>
2024-01-12 23:21:39 +0200

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,17 +1,20 @@
-const form = document.querySelector("form.login-form");
+const form = document.querySelector('form.login-form');
 
-form.addEventListener("submit", handleSubmit);
+form.addEventListener('submit', handleSubmit);
 
+const userData = {};
 
 function handleSubmit(event) {
   event.preventDefault();
   const form = event.target;
   const email = form.elements.email.value;
   const password = form.elements.password.value;
-  
-  if (email === "" || password === "") {
-    return console.log("Please fill in all the fields!");
+
+  if (email === '' || password === '') {
+    return alert('All form fields must be filled in');
   }
-      console.log(`Login: ${email.value}, Password: ${password.value}`);
+  userData.email = email.trim();
+  userData.password = password.trim();
+  console.log(userData);
   form.reset();
 }
