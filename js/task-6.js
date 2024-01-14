136d637a6f2114e2ef45d63eee9ba6c7863d05ad
vanisimov4 <144102084+vanisimov4@users.noreply.github.com>
2024-01-14 21:17:19 +0200

    StartTask-6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputNumber = document.querySelector('input');
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
+const divCollection = document.querySelector('div#boxes')
+
+console.log(divCollection);
+
+btnCreate.addEventListener('click', handleCreate);
+btnDestroy.addEventListener('click', handleDestroy);
+
+function handleCreate(event) { 
+  if (1 <= inputNumber && inputNumber <= 100) { 
+
+  }
+}
+
+function handleDestroy(event) { 
+
+}
+
+function createBoxes(amount) { 
+  const arrCollection = [];
+  let count = 0;
+
+while (count < amount) {
+  console.log(`Count: ${count}`);
+  arrCollection.push();
+  count += 1;
+}
+}
